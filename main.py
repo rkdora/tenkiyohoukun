d423fe3c380098eca310fed3c7fdafd22110b0b2
ryuto <rryuto5210@gmail.com>
2020-03-31 17:52:17 +0900

    Add import linebot.models

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage, TemplateSendMessage, ConfirmTemplate, MessageAction
+    MessageEvent, TextMessage, TextSendMessage, TemplateSendMessage, ConfirmTemplate, MessageAction, PostbackAction
 )
 import os
 
